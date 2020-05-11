922ea644411dcce70b9afc47cee44ca5dd02deee
Daniel Kwon <danielkwon89@gmail.com>
2020-05-11 16:33:06 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,12 +4,12 @@ def hello_t(array)
     while i < array.length
       yield array[i]
       i += 1
-    end
+    end #while loop
     array
   else
     puts "Hey! No block was given!"
-  end
-end
+  end #if/else statement
+end #hello_t method
 
 hello_t(["Tim", "Tom", "Jim"]) do |name|
     if name.start_with?("T")
